7487650b7f022c573fbda009b0817227d8966481
Andrew Curtis <curtis.andrewm@gmail.com>
2019-04-15 18:44:59 -0700

    Add passing unit tests for Region, Retailer, & Location models;add interface and start on service for Retailer

---
+++ rimrock_api/Models/Interfaces/IRetailer.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace rimrock_api.Models.Interfaces
+{
+	public interface IRetailer
+	{
+
+		Task<ActionResult<IEnumerable<Region>>> Get();
+
+		Task<ActionResult<Region>> Get(int id);
+	}
+}
---
+++ rimrock_api/Models/Services/RetailerService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using rimrock_api.Data;
+using rimrock_api.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace rimrock_api.Models.Services
+{
+	public class RetailerService : IRetailer
+	{
+
+		private readonly RimRockApiDbContext _context;
+
+		public RetailerService(RimRockApiDbContext context)
+		{
+			_context = context;
+		}
+
+		Task<ActionResult<IEnumerable<Region>>> IRetailer.Get()
+		{
+			throw new NotImplementedException();
+		}
+
+		Task<ActionResult<Region>> IRetailer.Get(int id)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
