124eb8eb662e534a48e636a928dbf521f66fcce3
Andrew Curtis <curtis.andrewm@gmail.com>
2019-04-16 09:38:40 -0700

    Add latest migration

--- rimrock_api/Migrations/20190415222400_initial.Designer.cs
+++ rimrock_api/Migrations/20190415222400_initial.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using rimrock_api.Data;
 
 namespace rimrock_api.Migrations
 {
     [DbContext(typeof(RimRockApiDbContext))]
-    partial class RimRockApiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190415222400_initial")]
+    partial class initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
